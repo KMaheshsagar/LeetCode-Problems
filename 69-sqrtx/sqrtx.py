b1481904d3d4ccbcc6cdba1b293b2063e1d5aa1a
KMaheshsagar <kmaheshsaga@gmail.com>
2026-09-06 22:59:01 +0530

    Time: 2 ms (75.86%) | Memory: 19.2 MB (56.67%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        low=0
+        high=x//2
+
+        while low<=high:
+            mid=low+(high-low)//2
+            square=mid*mid
+
+            if square==x:
+                return mid
+            elif square<x:
+                ans=mid
+                low=mid+1
+            else:
+                high=mid-1
+        return ans
