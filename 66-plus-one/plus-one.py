0963a21b30925934f05513865f83c6e06547c04b
KMaheshsagar <kmaheshsaga@gmail.com>
2026-09-23 23:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (20.02%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        n=len(digits)
+        for i in range(n-1,-1,-1):
+            digits[i]+=1
+            digits[i]%=10
+            if digits[i]!=0:
+                return digits
+        return [1]+digits
+        
